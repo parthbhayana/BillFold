9a468ad479f11c035914764154cc9b2925795385
nineleapskarthik <karthik.r@nineleaps>
2023-06-14 12:21:11 +0530

    added-refresh-token

--- src/main/java/com/nineleaps/expensemanagementproject/config/JwtUtil.java
+++ src/main/java/com/nineleaps/expensemanagementproject/config/JwtUtil.java
@@ -1,4 +1,5 @@
 package com.nineleaps.expensemanagementproject.config;
+
 import javax.servlet.http.*;
 import java.util.Date;
 import org.springframework.http.ResponseEntity;
@@ -6,28 +7,68 @@ import org.springframework.stereotype.Component;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+
 @Component
 public class JwtUtil {
-private static final String SECRET_KEY = "secret";
-public ResponseEntity<?> generateToken(String emailId, Long employeeId, String name, String imageUrl, String role,
-HttpServletResponse response) {
-String token = Jwts.builder().setSubject(emailId).claim("EmployeeID", employeeId)
-.claim("Role", role).setIssuedAt(new Date())
-.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
-.signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
-System.out.println(token);
-response.setHeader("Access_Token", token);
-return ResponseEntity.ok(token);
-}
-public boolean validateToken(String token) {
-try {
-Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
-return true;
-} catch (Exception e) {
-return false;
-}
-}
-public Claims getClaimsFromToken(String token) {
-return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
-}
+	private static final String SECRET_KEY = "secret";
+   
+    private static final long ACCESS_TOKEN_EXPIRATION_TIME = 1000 * 60 * 60 * 10; 
+    private static final long REFRESH_TOKEN_EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 30; 
+
+    public ResponseEntity<?> generateTokens(String emailId, Long employeeId, String name, String imageUrl, String role,
+                                            HttpServletResponse response) {
+        String accessToken = generateToken(emailId, employeeId, role, ACCESS_TOKEN_EXPIRATION_TIME);
+        String refreshToken = generateToken(emailId, employeeId, role, REFRESH_TOKEN_EXPIRATION_TIME);
+
+        response.setHeader("Access_Token", accessToken);
+        response.setHeader("Refresh_Token", refreshToken);
+
+        TokenResponse tokenResponse = new TokenResponse(accessToken, refreshToken);
+        return ResponseEntity.ok(tokenResponse);
+    }
+
+    private String generateToken(String emailId, Long employeeId, String role, long expirationTime) {
+        return Jwts.builder()
+                .setSubject(emailId)
+                .claim("EmployeeID", employeeId)
+                .claim("Role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Claims getClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+
+    private static class TokenResponse {
+        private String accessToken;
+        private String refreshToken;
+
+        public TokenResponse(String accessToken, String refreshToken) {
+            this.accessToken = accessToken;
+            this.refreshToken = refreshToken;
+        }
+
+        @SuppressWarnings("unused")
+		public String getAccessToken() {
+            return accessToken;
+        }
+
+        @SuppressWarnings("unused")
+		public String getRefreshToken() {
+            return refreshToken;
+        }
+    }
+
 }
