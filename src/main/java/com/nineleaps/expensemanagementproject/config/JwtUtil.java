01c9db8cadf411381d79f33459027f97334f7bed
parthbhayana <pbhayana007@gmail.com>
2023-06-14 13:20:00 +0530

    Security Depricated Code Changed

--- src/main/java/com/nineleaps/expensemanagementproject/config/JwtUtil.java
+++ src/main/java/com/nineleaps/expensemanagementproject/config/JwtUtil.java
@@ -1,33 +1,39 @@
 package com.nineleaps.expensemanagementproject.config;
+
 import javax.servlet.http.*;
 import java.util.Date;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+
 @Component
 public class JwtUtil {
-private static final String SECRET_KEY = "secret";
-public ResponseEntity<?> generateToken(String emailId, Long employeeId, String name, String imageUrl, String role,
-HttpServletResponse response) {
-String token = Jwts.builder().setSubject(emailId).claim("EmployeeID", employeeId)
-.claim("Role", role).setIssuedAt(new Date())
-.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
-.signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
-System.out.println(token);
-response.setHeader("Access_Token", token);
-return ResponseEntity.ok(token);
-}
-public boolean validateToken(String token) {
-try {
-Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
-return true;
-} catch (Exception e) {
-return false;
-}
-}
-public Claims getClaimsFromToken(String token) {
-return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
-}
+    private static final String SECRET_KEY = "secret";
+
+    public ResponseEntity<?> generateToken(String emailId, Long employeeId, String name, String imageUrl, String role,
+                                           HttpServletResponse response) {
+        String token = Jwts.builder().setSubject(emailId).claim("EmployeeID", employeeId)
+                .claim("Role", role).setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+        System.out.println(token);
+        response.setHeader("Access_Token", token);
+        return ResponseEntity.ok(token);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Claims getClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
 }
