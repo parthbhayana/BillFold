bd2144beafcea2b388f8fa11a9da229ff907e448
Parth Bhayana <110731373+parthbhayana@users.noreply.github.com>
2023-06-27 18:20:57 +0530

    Submit Report error fixed

--- src/main/java/com/nineleaps/expensemanagementproject/service/ReportsServiceImpl.java
+++ src/main/java/com/nineleaps/expensemanagementproject/service/ReportsServiceImpl.java
@@ -259,6 +259,10 @@ public class ReportsServiceImpl implements IReportsService {
             re.setDateSubmitted(today);
             re.setTotalAmountINR(totalamountINR(reportId));
             re.setTotalAmountCurrency(totalamountCurrency(reportId));
+            //Fetching employee ID
+            Long employeeId = re.getEmployeeId();
+            Employee employee = employeeServices.getEmployeeById(employeeId);
+            re.setManagerEmail(employee.getManagerEmail());
             reportsRepository.save(re);
 //			try {
 //				String decodedEmail = URLDecoder.decode(managerMail, "UTF-8");
