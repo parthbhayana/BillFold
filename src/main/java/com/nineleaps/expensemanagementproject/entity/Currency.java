c9bd3f43e883c7ae2f6064a05097254d459561dc
parthbhayana <pbhayana007@gmail.com>
2023-08-21 18:09:19 +0530

    Currency Symbol Added

--- src/main/java/com/nineleaps/expensemanagementproject/entity/Currency.java
+++ src/main/java/com/nineleaps/expensemanagementproject/entity/Currency.java
@@ -2,7 +2,16 @@ package com.nineleaps.expensemanagementproject.entity;
 
 public enum Currency {
 
-	EUR, USD, JPY, BGN, CZK, DKK, GBP, HUF, PLN, RON, SEK, CHF, ISK, NOK, HRK, RUB, TRY, AUD, BRL, CAD, CNY, HKD, IDR,
-	ILS, INR, KRW, MXN, MYR, NZD, PHP, SGD, THB, ZAR
+    EUR("€"), USD("$"), JPY("¥"), BGN("лв"), CZK("Kč"), DKK("kr"), GBP("£"), HUF("Ft"), PLN("zł"), RON("lei"), SEK("kr"), CHF("Fr"), ISK("kr"), NOK("kr"), HRK("kn"), RUB("₽"), TRY("₺"), AUD("$"), BRL("R$"), CAD("$"), CNY("¥"), HKD("$"), IDR("Rp"), ILS("₪"), INR("₹"), KRW("₩"), MXN("$"), MYR("RM"), NZD("$"), PHP("₱"), SGD("$"), THB("฿"), ZAR("R");
+
+    private final String symbol;
+
+    Currency(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
 
 }
