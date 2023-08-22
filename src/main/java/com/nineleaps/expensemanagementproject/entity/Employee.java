bb814773826764d0a9e0a7e77aebe0c61199728a
parthbhayana <pbhayana007@gmail.com>
2023-08-22 12:19:37 +0530

    Notify LnD & Reject Report Bugs Fixed

--- src/main/java/com/nineleaps/expensemanagementproject/entity/Employee.java
+++ src/main/java/com/nineleaps/expensemanagementproject/entity/Employee.java
@@ -11,230 +11,256 @@ import io.swagger.annotations.ApiModelProperty;
 @Table(name = "employee")
 public class Employee {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@ApiModelProperty(hidden = true)
-	@Column(name = "employee_id")
-	private Long employeeId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @ApiModelProperty(hidden = true)
+    @Column(name = "employee_id")
+    private Long employeeId;
 
-	@Column(name = "official_id")
-	private String officialEmployeeId;
+    @Column(name = "official_id")
+    private String officialEmployeeId;
 
-	@Column(name = "first_name")
-	private String firstName;
+    @Column(name = "first_name")
+    private String firstName;
 
-	@Column(name = "middle_name")
-	private String middleName;
+    @Column(name = "middle_name")
+    private String middleName;
 
-	@Column(name = "last_name")
-	private String lastName;
+    @Column(name = "last_name")
+    private String lastName;
 
-	@Column(name = "employee_email")
-	private String employeeEmail;
+    @Column(name = "employee_email")
+    private String employeeEmail;
 
-	@Column(name = "manager_email")
-	private String managerEmail;
+    @Column(name = "manager_email")
+    private String managerEmail;
 
-	@Column(name = "manager_name")
-	@ApiModelProperty(hidden = true)
-	private String managerName;
+    @Column(name = "manager_name")
+    @ApiModelProperty(hidden = true)
+    private String managerName;
 
-	@Column(name = "hr_name")
-	@ApiModelProperty(hidden = true)
-	private String hrName;
+    @Column(name = "hr_name")
+    @ApiModelProperty(hidden = true)
+    private String hrName;
 
-	@Column(name = "hr_email")
-	private String hrEmail;
+    @Column(name = "hr_email")
+    private String hrEmail;
 
-	@Column(name = "mobile_number")
-	private Long mobileNumber;
+    @Column(name = "lnd_name")
+    @ApiModelProperty(hidden = true)
+    private String lndName = "Vrusha";
 
-	@Column(name = "is_admin", nullable = true)
-	@ApiModelProperty(hidden = true)
-	private Boolean isFinanceAdmin = false;
+    @Column(name = "lnd_email")
+    @ApiModelProperty(hidden = true)
+    private String lndEmail = "billfoldfinancejsr@gmail.com";
 
-	@Column(name = "image_url")
-	@ApiModelProperty(hidden = true)
-	private String imageUrl;
+    @Column(name = "mobile_number")
+    private Long mobileNumber;
 
-	@Column(name = "is_hidden", nullable = true)
-	@ApiModelProperty(hidden = true)
-	private Boolean isHidden = false;
+    @Column(name = "is_admin", nullable = true)
+    @ApiModelProperty(hidden = true)
+    private Boolean isFinanceAdmin = false;
 
-	@JsonIgnore
-	@OneToMany(mappedBy = "employee", cascade = CascadeType.ALL)
-	private List<Expense> expenseList = new ArrayList<>();
+    @Column(name = "image_url")
+    @ApiModelProperty(hidden = true)
+    private String imageUrl;
 
-	@Column(name = "role")
-	@ApiModelProperty(hidden = true)
-	private String role = "EMPLOYEE";
+    @Column(name = "is_hidden", nullable = true)
+    @ApiModelProperty(hidden = true)
+    private Boolean isHidden = false;
 
-	@Column(name = "fcm_token")
-	@ApiModelProperty(hidden = true)
-	private String token;
+    @JsonIgnore
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL)
+    private List<Expense> expenseList = new ArrayList<>();
 
-	public Employee() {
+    @Column(name = "role")
+    @ApiModelProperty(hidden = true)
+    private String role = "EMPLOYEE";
 
-	}
+    @Column(name = "fcm_token")
+    @ApiModelProperty(hidden = true)
+    private String token;
 
-	public Employee(Long employeeId, String officialEmployeeId, String firstName, String middleName, String lastName,
-			String employeeEmail, String managerEmail, String managerName, String hrName, String hrEmail,
-			Long mobileNumber, Boolean isFinanceAdmin, String imageUrl, Boolean isHidden, List<Expense> expenseList,
-			String role, String token) {
-		super();
-		this.employeeId = employeeId;
-		this.officialEmployeeId = officialEmployeeId;
-		this.firstName = firstName;
-		this.middleName = middleName;
-		this.lastName = lastName;
-		this.employeeEmail = employeeEmail;
-		this.managerEmail = managerEmail;
-		this.managerName = managerName;
-		this.hrName = hrName;
-		this.hrEmail = hrEmail;
-		this.mobileNumber = mobileNumber;
-		this.isFinanceAdmin = isFinanceAdmin;
-		this.imageUrl = imageUrl;
-		this.isHidden = isHidden;
-		this.expenseList = expenseList;
-		this.role = role;
-		this.token = token;
-	}
+    public Employee() {
 
-	public Long getEmployeeId() {
-		return employeeId;
-	}
+    }
 
-	public void setEmployeeId(Long employeeId) {
-		this.employeeId = employeeId;
-	}
+    public Employee(Long employeeId, String officialEmployeeId, String firstName, String middleName, String lastName,
+                    String employeeEmail, String managerEmail, String managerName, String hrName, String hrEmail,
+                    String lndName, String lndEmail, Long mobileNumber, Boolean isFinanceAdmin, String imageUrl,
+                    Boolean isHidden, List<Expense> expenseList, String role, String token) {
+        super();
+        this.employeeId = employeeId;
+        this.officialEmployeeId = officialEmployeeId;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.employeeEmail = employeeEmail;
+        this.managerEmail = managerEmail;
+        this.managerName = managerName;
+        this.hrName = hrName;
+        this.hrEmail = hrEmail;
+        this.lndName = lndName;
+        this.lndEmail = lndEmail;
+        this.mobileNumber = mobileNumber;
+        this.isFinanceAdmin = isFinanceAdmin;
+        this.imageUrl = imageUrl;
+        this.isHidden = isHidden;
+        this.expenseList = expenseList;
+        this.role = role;
+        this.token = token;
+    }
 
-	public String getOfficialEmployeeId() {
-		return officialEmployeeId;
-	}
+    public Long getEmployeeId() {
+        return employeeId;
+    }
 
-	public void setOfficialEmployeeId(String officialEmployeeId) {
-		this.officialEmployeeId = officialEmployeeId;
-	}
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getOfficialEmployeeId() {
+        return officialEmployeeId;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public void setOfficialEmployeeId(String officialEmployeeId) {
+        this.officialEmployeeId = officialEmployeeId;
+    }
 
-	public String getMiddleName() {
-		return middleName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public void setMiddleName(String middleName) {
-		this.middleName = middleName;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getMiddleName() {
+        return middleName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
 
-	public String getEmployeeEmail() {
-		return employeeEmail;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public void setEmployeeEmail(String employeeEmail) {
-		this.employeeEmail = employeeEmail;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public String getManagerEmail() {
-		return managerEmail;
-	}
+    public String getEmployeeEmail() {
+        return employeeEmail;
+    }
 
-	public void setManagerEmail(String managerEmail) {
-		this.managerEmail = managerEmail;
-	}
+    public void setEmployeeEmail(String employeeEmail) {
+        this.employeeEmail = employeeEmail;
+    }
 
-	public String getManagerName() {
-		return managerName;
-	}
+    public String getManagerEmail() {
+        return managerEmail;
+    }
 
-	public void setManagerName(String managerName) {
-		this.managerName = managerName;
-	}
+    public void setManagerEmail(String managerEmail) {
+        this.managerEmail = managerEmail;
+    }
 
-	public String getHrName() {
-		return hrName;
-	}
+    public String getManagerName() {
+        return managerName;
+    }
 
-	public void setHrName(String hrName) {
-		this.hrName = hrName;
-	}
+    public void setManagerName(String managerName) {
+        this.managerName = managerName;
+    }
 
-	public String getHrEmail() {
-		return hrEmail;
-	}
+    public String getHrName() {
+        return hrName;
+    }
 
-	public void setHrEmail(String hrEmail) {
-		this.hrEmail = hrEmail;
-	}
+    public void setHrName(String hrName) {
+        this.hrName = hrName;
+    }
 
-	public Long getMobileNumber() {
-		return mobileNumber;
-	}
+    public String getHrEmail() {
+        return hrEmail;
+    }
 
-	public void setMobileNumber(Long mobileNumber) {
-		this.mobileNumber = mobileNumber;
-	}
+    public void setHrEmail(String hrEmail) {
+        this.hrEmail = hrEmail;
+    }
 
-	public Boolean getIsFinanceAdmin() {
-		return isFinanceAdmin;
-	}
+    public String getLndName() {
+        return lndName;
+    }
 
-	public void setIsFinanceAdmin(Boolean isFinanceAdmin) {
-		this.isFinanceAdmin = isFinanceAdmin;
-	}
+    public void setLndName(String lndName) {
+        this.lndName = lndName;
+    }
 
-	public String getImageUrl() {
-		return imageUrl;
-	}
+    public String getLndEmail() {
+        return lndEmail;
+    }
 
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
+    public void setLndEmail(String lndEmail) {
+        this.lndEmail = lndEmail;
+    }
 
-	public Boolean getIsHidden() {
-		return isHidden;
-	}
+    public Long getMobileNumber() {
+        return mobileNumber;
+    }
 
-	public void setIsHidden(Boolean isHidden) {
-		this.isHidden = isHidden;
-	}
+    public void setMobileNumber(Long mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
 
-	public List<Expense> getExpenseList() {
-		return expenseList;
-	}
+    public Boolean getIsFinanceAdmin() {
+        return isFinanceAdmin;
+    }
 
-	public void setExpenseList(List<Expense> expenseList) {
-		this.expenseList = expenseList;
-	}
+    public void setIsFinanceAdmin(Boolean isFinanceAdmin) {
+        this.isFinanceAdmin = isFinanceAdmin;
+    }
 
-	public String getRole() {
-		return role;
-	}
+    public String getImageUrl() {
+        return imageUrl;
+    }
 
-	public void setRole(String role) {
-		this.role = role;
-	}
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 
-	public String getToken() {
-		return token;
-	}
+    public Boolean getIsHidden() {
+        return isHidden;
+    }
 
-	public void setToken(String token) {
-		this.token = token;
-	}
+    public void setIsHidden(Boolean isHidden) {
+        this.isHidden = isHidden;
+    }
+
+    public List<Expense> getExpenseList() {
+        return expenseList;
+    }
+
+    public void setExpenseList(List<Expense> expenseList) {
+        this.expenseList = expenseList;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 
 }
